83f380f2c2a85ce8d78ef377dd0239605b118b83
(Abe) Khalil Saghiri <khalilsa01@gmail.com>
2022-11-17 02:41:16 +0200

    Evaluate and score the board, Minimax prototype

--- Tba/project.c
+++ Tba/project.c
@@ -17,25 +17,26 @@
 #define COLS 7
 #define MAX_INPUT 20
 
-#define AI_PIECE 1
-#define PLAYER_PIECE 2
-
 enum MODE {PLAYER_VS_PLAYER, PLAYER_VS_AI};
 enum TOKEN {EMPTY, RED, YELLOW};
 enum AGENT {PLAYER, AI};
 enum AI_DIFFICULTY {RANDOM, EASY = 2, MEDIUM = 4, HARD = 6};
 
-int column = 0;
-char token = RED;
-int player = PLAYER;
-int difficulty = RANDOM;
+int g_column = 0;
+int g_token = RED;
+int g_player = PLAYER;
+int g_difficulty = EASY;
+int g_ai_piece, g_player_piece;
+
+// track how many moves the players have made so far
+unsigned int g_moves = 0;
 
 // Main functions
 
 void init_board(int** board);
 void choose(int** board, int mode);
 void fill_column(int **board, int column, int player);
-int check_winner(int **board, char token);
+int check_winner(int **board, int token);
 double choose_with_timer(int **board, int mode, void (*choose)(int** board, int mode));
 
 // String/UI functions  
@@ -49,16 +50,18 @@ int check_valid_name(char *name);
 // helper functions
 
 int generate_random_int(int lower, int upper);
-void copy_board(int** board, int** board_copy);
+void copy_board(int** src_board, int** dist_board);
 void string_to_lower_case(char* str);
+int count_token(int* arr, int token, int start, int end);
 
 // Bot functions
 
+int make_move(int **board);
 void minimax(int **board, int depth, int alpha, int beta, int is_maximazing_player, int *column, int *score);
 int* get_valid_locations(int **board);
-int score_board(int** board, int player);
+int score_board(int** board, int token);
 int random_valid_column(int* valid_columns);
-int get_open_row(int** board, int column);
+int score_bucket(int* bucket, int token, int start, int end);
 
 int main() {
     // Allocate board in the stack
@@ -119,19 +122,20 @@ int main() {
     print_dashed_line(40);
     
     // Randomly pick the one who starts first
-    player = generate_random_int(PLAYER, AI);
+    g_player = generate_random_int(PLAYER, AI);
     if (mode == PLAYER_VS_AI) {
-        if (player == AI) {
+        if (g_player == AI) {
             strncpy(name2, name1, MAX_INPUT);
             strncpy(name1, "Computer", MAX_INPUT);
+            g_player_piece = YELLOW;
+            g_ai_piece = RED;
         } else {
             strncpy(name2, "Computer", MAX_INPUT);
+            g_player_piece = RED;
+            g_ai_piece = YELLOW;
         }
     }
     
-
-    // track how many moves the players have made so far
-    unsigned int moves = 0;
     double total_time_red = 0;
     double total_time_yellow = 0;
 
@@ -143,7 +147,7 @@ int main() {
     while (1) {
         // announce the winner in case of a tie (the board is full but no winner)
         // the winner in that case is the one who took less time
-        if (moves >= ROWS * COLS) {
+        if (g_moves >= ROWS * COLS) {
             if (total_time_red == total_time_yellow) {
                 printf("It's a tie. both players took %.2f seconds\n", total_time_red);
             } else {
@@ -154,26 +158,26 @@ int main() {
             break;
         }
         // Gameplay Code
-        printf("\n%s's turn.\n", (token == 1) ? name1 : name2);
-        if (token == RED) {
+        printf("\n%s's turn.\n", (g_token == RED) ? name1 : name2);
+        if (g_token == RED) {
             total_time_red += choose_with_timer(board, mode, choose);
-        } else if (token == YELLOW) {
+        } else if (g_token == YELLOW) {
             total_time_yellow += choose_with_timer(board, mode, choose);
         }
         
         printf("\n\n");
 
         print_board(board);
-        if (check_winner(board, 1)) {
+        if (check_winner(board, RED)) {
             printf("\n\%s is the winner!\n\n", name1);
             break;  
         }
-        if (check_winner(board, 2)) {
+        if (check_winner(board, YELLOW)) {
             printf("\n\%s is the winner!\n\n", name2);
             break;  
         }
 
-        moves++;
+        g_moves++;
     }
     return 0; 
 }
@@ -202,8 +206,9 @@ void print_board(int** board) {
 // choose a column to drop the token in so that the token falls to the bottom of the column depending on the current state of the board
 void choose(int** board, int mode) {
     char column_user_input[MAX_INPUT];
+    int col;
     while (1) {
-        if (player == PLAYER || mode != PLAYER_VS_AI) {
+        if (g_player == PLAYER || mode != PLAYER_VS_AI) {
             printf("\nChoose column: ");
             fgets(column_user_input, sizeof(column_user_input), stdin);
             trim_trailing(column_user_input);
@@ -213,7 +218,7 @@ void choose(int** board, int mode) {
                 continue;
             }
             
-            int col = atoi(column_user_input) - 1;
+            col = atoi(column_user_input) - 1;
 
             if (col < 0 || col > 6) {
                 puts("Invalid Column. please enter a column between 1-7.");
@@ -224,17 +229,26 @@ void choose(int** board, int mode) {
                 puts("COLUMN IS FULL, Please choose another column.");
                 continue;
             }
-            fill_column(board, col, token);
-        } else if (player == AI) {
-            if (difficulty == RANDOM) {
-                int col = generate_random_int(0, 7);
-                printf("Computer placed in column %d\n", col);
-                fill_column(board, col, token);
+            fill_column(board, col, g_token);
+        } else if (g_player == AI) {
+            if (g_difficulty == RANDOM) {
+                col = generate_random_int(0, 7);
+                printf("Computer placed in column %d\n", col + 1);
+                fill_column(board, col, g_token);
+            } else {
+                col = make_move(board);
+                if (col != -1) {
+                    printf("Computer placed in column %d\n", col + 1);
+                    fill_column(board, col, g_token);
+                }
             }
         }
         
-        token = (token == RED) ? YELLOW : RED;
-        player = (player == AI) ? PLAYER : AI;
+        g_token = (g_token == RED) ? YELLOW : RED;
+        g_player = (g_player == AI) ? PLAYER : AI;
+        g_ai_piece = (g_ai_piece == RED) ? YELLOW : RED;
+        g_player_piece = (g_player_piece == RED) ? YELLOW : RED;
+
         break;
     }
 }
@@ -243,10 +257,10 @@ void choose(int** board, int mode) {
     Requires: column is a valid column number (0-6) and the column is not full (the top of the column is empty) 
     Effects: fills the column with the current token (1 or 2) and drops the token to the bottom of the column
 */
-void fill_column(int **board, int column, int player) {
+void fill_column(int **board, int column, int token) {
     for (int i = ROWS - 1; i >= 0; i--) {
         if (board[i][column] == EMPTY) {
-            board[i][column] = player;
+            board[i][column] = token;
             break;
         }
     }               
@@ -257,7 +271,7 @@ void fill_column(int **board, int column, int player) {
     Effects: checks if the current player has won the game by checking all possible winning 
     combinations row, column, and diagonals then returns 1 if the player has won and 0 otherwise
 */
-int check_winner(int **board, char token) {
+int check_winner(int **board, int token) {
 
     // horizontal Checking
     for (int j = 0; j < COLS-3 ; j++ ) {
@@ -395,30 +409,49 @@ void string_to_lower_case(char* str) {
     }
 }
 
-// /*
-//     Makes an intellegent move
-// */
-// int make_move(int **board) {
-//     int column;
-//     int score;
-//     minimax(**board, depth, alpha, beta, AI_PIECE, &column, &score);
-//     return column;
-// }
+/*
+    Makes an intellegent move
+*/
+int make_move(int **board) {
+    int column;
+    int score;
+    minimax(board, g_difficulty, -__INT_MAX__, __INT_MAX__, g_ai_piece, &column, &score);
+    // int *valid_locations = get_valid_locations(board);
+    // int best_score = 0;
+    // int best_col = random_valid_column(valid_locations);
+    // for (int col = 0; col < COLS; col++) {
+    //     if (!valid_locations[col]) continue;
+    //     int** board_copy = (int**) alloca(ROWS * sizeof(int *));
+    //     for (int i = 0; i < ROWS; i++) {
+    //         board_copy[i] = (int *) alloca(COLS * sizeof(int));
+    //     }
+    //     copy_board(board, board_copy);
+    //     fill_column(board_copy, col, g_ai_piece);
+    //     int score = score_board(board_copy, g_ai_piece);
+    //     if (score > best_score) {
+    //         best_score = score;
+    //         best_col = col;
+    //     }
+    // }
+    // return best_col;
+    return column;
+}
 
 void minimax(int **board, int depth, int alpha, int beta, int is_maximazing_player, int *column, int *score) {
     int *valid_locations = get_valid_locations(board);
-    int ai_won = check_winner(board, AI_PIECE);
-    int player_won = check_winner(board, PLAYER_PIECE);
-    int is_game_over = ai_won || player_won;
+    int ai_won = check_winner(board, g_ai_piece);
+    int player_won = check_winner(board, g_player_piece);
+    int board_full = g_moves >= ROWS * COLS ? 1 : 0;
+    int is_game_over = ai_won || player_won || board_full;
     if (depth == 0 || is_game_over) {
         if (is_game_over) { 
             if (ai_won) {
                 *column = -1;
-                *score  = 1000;
+                *score  = 10000;
             }
             else if (player_won) {
                 *column = -1;
-                *score  = -1000;
+                *score  = -10000;
             } 
             // draw
             else {
@@ -429,7 +462,7 @@ void minimax(int **board, int depth, int alpha, int beta, int is_maximazing_play
         // depth = 0
         else {
             *column = -1;
-            *score  = score_board(board, AI_PIECE);
+            *score  = score_board(board, g_ai_piece);
         }
         return;
     }
@@ -444,7 +477,7 @@ void minimax(int **board, int depth, int alpha, int beta, int is_maximazing_play
                     board_copy[i] = (int *) alloca(COLS * sizeof(int));
                 }
                 copy_board(board, board_copy);
-                fill_column(board_copy, *column, AI_PIECE);
+                fill_column(board_copy, *column, g_ai_piece);
                 int new_score = 0;
                 int new_column = 0;
                 minimax(board_copy, depth - 1, alpha, beta, 0, &new_column, &new_score);
@@ -467,7 +500,7 @@ void minimax(int **board, int depth, int alpha, int beta, int is_maximazing_play
                     board_copy[i] = (int *) alloca(COLS * sizeof(int));
                 }
                 copy_board(board, board_copy);
-                fill_column(board_copy, *column, PLAYER_PIECE);
+                fill_column(board_copy, *column, g_player_piece);
                 int new_score = 0;
                 int new_column = 0;
                 minimax(board_copy, depth - 1, alpha, beta, 1, &new_column, &new_score);
@@ -503,18 +536,110 @@ int random_valid_column(int* valid_columns) {
     while (i < COLS || !valid_columns[random_col]) {
         random_col++;
         random_col %= COLS;
+        i++;
     }
     return random_col;
 }
 
-void copy_board(int** board, int** board_copy) {
+void copy_board(int** src_board, int** dist_board) {
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            board_copy[i][j] = board[i][j];
+            dist_board[i][j] = src_board[i][j];
         }
     }
 }
 
-int score_board(int** board, int player) {
-    return 0;
+int score_board(int** board, int token) {
+    int score = 0;
+    // Score Center Column
+    int center_column[ROWS];
+    for (int i = 0; i < ROWS; i++) {
+        int mid = COLS / 2;
+        center_column[i] = board[i][mid];
+    }
+    int center_tokens_num = count_token(center_column, token, 0, ROWS);
+    score += center_tokens_num * 6;
+    // Score Horizontal
+    for (int i = 0; i < ROWS; i++) {
+        int* row_array = board[i];
+        for (int j = 0; j < COLS - 3; j++) {
+            int start = j;
+            int end = j + 4;
+            score += score_bucket(row_array, token, start, end);
+        }
+    }
+
+    // Score Vertical
+    for (int j = 0; j < COLS; j++) {
+        int col_array[ROWS];
+        for (int i = 0; i < ROWS; i++) {
+            col_array[i] = board[i][j];
+        }
+        for (int i = 0; i < ROWS - 3; i++) {
+            int start = j;
+            int end = j + 4;
+            score += score_bucket(col_array, token, start, end);
+        }
+    }
+
+    // score Ascending diagonals
+    // ascending Diagonal Check
+    for (int i=3; i < ROWS; i++) {
+        for (int j=0; j < COLS-3; j++) {
+            int diagonal_bucket[4];
+            for (int k = 0; k < 4; k++) {
+                diagonal_bucket[k] = board[i-k][j+k];
+            }
+            score += score_bucket(diagonal_bucket, token, 0, 4);
+            
+        }
+    }
+
+    // descending Diagonal Check
+    for (int i=3; i < ROWS; i++) {
+        for (int j=3; j < COLS; j++) {
+            int diagonal_bucket[4];
+            for (int k = 0; k < 4; k++) {
+                diagonal_bucket[k] = board[i-k][j-k];
+            }
+            score += score_bucket(diagonal_bucket, token, 0, 4);
+        }
+    }
+    return score;
+}
+
+int score_bucket(int* bucket, int token, int start, int end) {
+    int opp_piece = g_player_piece;
+    if (token == g_player_piece) {
+        opp_piece = g_ai_piece;
+    }
+    int score = 0;
+
+    int token_num = count_token(bucket, token, start, end);
+    int empty_num = count_token(bucket, EMPTY, start, end);
+    int opp_token_num = count_token(bucket, opp_piece, start, end);
+    if (token_num == 4) {
+        score += 10000;
+    }
+    else if (token_num == 3 && empty_num == 1) {
+        score += 10;
+    }
+    else if (token_num == 2 && empty_num == 2) {
+        score += 5;
+    }
+    else if (opp_token_num == 3 && empty_num == 1) {
+        score -= 100;
+    }
+
+    return score;
+}
+
+int count_token(int* arr, int token, int start, int end) {
+    int count = 0;
+    for (int i = start; i < end; i++) {
+        if (arr[i] == token) {
+            count++;
+        }
+    }
+    return count;
 }
